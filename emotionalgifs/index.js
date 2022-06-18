4bb8a88c869c43d26a4981106dd69769942e3ac0
Jack Shunn <jack.shunn@gmail.com>
2022-06-17 20:58:18 -0600

    Add image ai

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,4 +1,5 @@
-const multipart = require("parse-multipart")
+const multipart = require("parse-multipart");
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -9,11 +10,29 @@ module.exports = async function (context, req) {
     const body = req.body;
 
     const parts = multipart.Parse(body, boundary);
-
-    const convertedResult = Buffer.from(parts[0].data).toString("base64");
+    
+    const emotion_result = await analyzeImage(parts[0].data);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
-    };
+        body: {emotion_result}
+    }
+}
+
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+    })
+
+    const emotion_result = await fetch(uriBase +"?"+ params.toString(), {
+        method: "POST",
+        body: img,
+        headers: { "Content-Type": "application/octet-stream",
+                "Ocp-Apim-Subscription-Key":subscriptionKey}
+    })
+    const data = await emotion_result.json();
+    return data;
 }
