916def1858af575d2d5159d55ab2f8ed6a754e0b
Jack Shunn <jack.shunn@gmail.com>
2022-06-25 11:34:09 -0600

    added song recomendation

--- song4u/index.js
+++ song4u/index.js
@@ -2,6 +2,12 @@ const querystring = require('qs');
 const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+    "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+    "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+    "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+    "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
     const queryObject = querystring.parse(req.body);
     const imageUrl = queryObject.MediaUrl0;
     
@@ -23,7 +29,7 @@ module.exports = async function (context, req) {
         result = 'BabyBoomers'
         
     context.res = {
-        body: result
+        body: `We guessed you're part of this generation: ${result}! Happy listening! ${songs[result]}`
     };
 }
 
