583ea58977fd8ba66c09cef79776c7d43a42b992
Jack Shunn <jack.shunn@gmail.com>
2022-06-28 23:33:33 -0600

    added file upload box

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -2,7 +2,17 @@ const bunnForm = document.getElementById('bunnForm');
 
 bunnForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const username = document.getElementById('username').value;
-    const output = document.getElementById('output');
-    output.textContent = `${username}❤`;
+    const fileUpload = document.getElementById('fileUpload');
+    if (!/^.*\.(png|jpg|jpeg)$/.test(fileUpload.value)) {
+        alert("Invalid File");
+        return;
+    }
+
+    const fileName = document.getElementById('filename');
+    if (!fileName.value){
+        alert("No name given");
+        return;
+    }
+
+    document.getElementById('output').textContent = 'Thanks!';
 });
