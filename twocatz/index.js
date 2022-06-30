858ba4f7bfa0e395858a627b7fddef971fcb44a5
Jack Shunn <jack.shunn@gmail.com>
2022-06-30 01:10:31 -0600

    4 cats

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,31 +3,26 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    cat1_64 = await FetchCat64();
-    cat2_64 = await FetchCat64();
+    let cat1_64 = await FetchCat64(req.query.name1);
+    let cat2_64 = await FetchCat64(req.query.name2);
+    let cat3_64 = await FetchCat64(req.query.name3);
+    let cat4_64 = await FetchCat64(req.query.name4);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
-            cat1: cat1_64,
-            cat2: cat2_64,
-            names : TwoNames()
+            name1: cat1_64,
+            name2: cat2_64,
+            name3: cat3_64,
+            name4: cat4_64,
         }
     };
 }
 
-async function FetchCat64(){
-    const cat_request = await fetch("https://cataas.com/cat/cute/says/Bitcamp",
-    {method:"GET"}
-    )
+async function FetchCat64(text){
+    const cat_request = await fetch(`https://cataas.com/cat/cute/says/${text}`,{method:"GET"});
     const cat_buffer = await cat_request.arrayBuffer();
 
     const cat_base64 = Buffer.from(cat_buffer).toString("base64");
     return cat_base64;
-}
-
-function TwoNames() {
-    const names = ['Shreya', 'Emily', 'Fifi', 'Beau', 'Evelyn', 'Julia', 'Daniel', 'Fardeen'];
-
-    return [names[Math.floor(Math.random()*names.length)], names[Math.floor(Math.random()*names.length)]]
 }
